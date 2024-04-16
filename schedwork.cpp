3a6ca41c91f2877bcb52ec952775a507d0b3c13a
Lauren Campbell <laurencc@usc.edu>
2024-04-16 08:28:56 +0000

    Finished PA #5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t d, const size_t m, DailySchedule& sched, size_t currN, size_t currD, size_t prevK, std::map<Worker_T, size_t> shiftCt);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +37,46 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    // initializing default sched (a bunch of zeroes)
+    for (size_t i = 0; i < avail.size(); i++) {
+      std::vector<Worker_T> temp(dailyNeed, 0);
+      sched.push_back(temp);
+    }
 
+    // initializing initial shift counts for each worker
+    const size_t numWorkers = avail[0].size();
+    std::map<Worker_T, size_t> shiftCt;
+    for (size_t i = 0; i < numWorkers; i++) {
+      shiftCt[i] = 0;
+    }
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, -1, shiftCt);
+}
 
+bool scheduleHelper(const AvailabilityMatrix& avail, const size_t d, const size_t m, DailySchedule& sched, size_t currN, size_t currD, size_t prevK, std::map<Worker_T, size_t> shiftCt)
+{
+  // if all days have been filled out
+  if (currN == avail.size())
+    return true;
+  // if number of workers has been met
+  else if (currD == d) 
+    return scheduleHelper(avail, d, m, sched, currN+1, 0, -1, shiftCt);
 
-
+  else {
+    // iterating through workers
+    for (size_t i = 0; i < avail[0].size(); i++) {
+      // if worker is available on this day
+      if (avail[currN][i] && shiftCt[i] < m && i != prevK) {
+        sched[currN][currD] = i;
+        shiftCt[i] += 1;
+        
+        // if program does not backtrack
+        if (scheduleHelper(avail, d, m, sched, currN, currD+1, i, shiftCt))
+          return true;
+        // else, revert shift count
+        shiftCt[i] -= 1;
+      }
+    }
+  }
+  return false;
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,9 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void getWords(std::set<std::string>& words, std::string in, std::string floating, int len);
+int numHyphens(const std::string& in);
+std::set<std::string> validWords(std::set<std::string>& words, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +24,64 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> words;
+    getWords(words, in, floating, 0);
+    return validWords(words, dict);
 }
 
 // Define any helper functions here
+void getWords(std::set<std::string>& words, std::string in, std::string floating, int len) 
+{
+    // if the word is finished
+    if (len == in.size() && floating == "")
+        words.insert(in);
+    // if all letters have been filled but floating requirement has not been met
+    else if (len == in.size())
+      return;
+    // if letter is fixed
+    else if (in[len] != '-')
+        getWords(words, in, floating, len+1);
+    
+    else {
+      // if there is only room left for the floating letters, search floating letters only
+        std::string alphabet;
+        if (numHyphens(in) == floating.size()) 
+          alphabet = floating;
+        else
+          alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < alphabet.size(); i++) {
+            in[len] = alphabet[i];
+            int floatIdx = floating.find(alphabet[i]);
+
+            // checking off floating letters
+            if (floatIdx != -1) {
+                std::string newFloating(floating);
+                newFloating.erase(newFloating.begin() + floatIdx);
+                getWords(words, in, newFloating, len+1);
+            }
+            else 
+                getWords(words, in, floating, len+1);
+        }
+    }
+}
+
+int numHyphens(const std::string& in)
+{
+  int num = 0;
+  for (int i = 0; i < in.size(); i++) {
+    if (in[i] == '-')
+      num++;
+  }
+  return num;
+}
+
+std::set<std::string> validWords(std::set<std::string>& words, const std::set<std::string>& dict)
+{
+    std::set<std::string> validWords;
+    for (std::set<std::string>::iterator it = words.begin(); it != words.end(); ++it) {
+        if (dict.find(*it) != dict.end())
+            validWords.insert(*it);
+    }
+    return validWords;
+}
